9d859b6b9d10e852ca81b9cc4603589f43760ffa
fabioscielzo <fabioscielzo98@gmail.com>
2023-05-13 19:11:04 +0200

    n

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="<https://github.com/FabioScielzoOrtiz/Distances-Python-Package>",  # add your project URL here
+    url="<https://github.com/FabioScielzoOrtiz/Distances_Package>",  # add your project URL here
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
